161ba39f558d622550440f55445c1e7658af03eb
Konstantin <russiankoteyka@gmail.com>
2019-12-15 19:13:26 +0300

    работающий вариант, сбольшим количеством копирований

--- lab4/gen.py
+++ lab4/gen.py
@@ -1,10 +1,10 @@
 import warnings
 import numpy as np
 
-n = 1000
+n = 300
 
 print(n)
-a = np.random.uniform(0, 0.001, (n, n))
+a = np.random.uniform(0, 1, (n, n))
 for i in range(n):
     for j in range(n):
         print(a[i][j], end=' ')
--- lab4/lab4.cu
+++ lab4/lab4.cu
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <string.h>
 
-#define MAX_N 1000
+#define MAX_N 5000
 
 #define CSC(call)                                                    \
 do {                                                                \
@@ -28,25 +28,28 @@ __global__ void kernel(double* dev_a, int i, int n){
         ratio = dev_a[y * MAX_N + i] / cur_row[i];
         for (int x = i + 1 + start_x; x < n; x += offset_x){
             dev_a[y * MAX_N + x] -= ratio * cur_row[x];
-            // printf("upd %d %d\n",x, y );
         }
     }
 }
 double a[MAX_N][MAX_N];
+// __device__ static double dev_a[MAX_N][MAX_N];
 int cur_i;
 bool cmp(int el1, int el2){
-    // компаратор -- сравнивает значения в столбцах у строк по заданным значениям
+    // компаратор -- сравнивает значения в столбце у строк по заданным значениям
     // номер столбца задаётся глобально
     return abs(a[el1][cur_i]) < abs(a[el2][cur_i]);
 }
-int main() {
-     // double a[10000][10000];
+int main(int argc, char const *argv[]) {
+    FILE* inp = stdin;
+    if (argc > 1)
+        inp = fopen(argv[1], "r");
+    
     int n;
     double eps = 1e-7;
-    scanf("%d", &n);
+    fscanf(inp, "%d", &n);
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
-            scanf("%lf", &a[i][j]);
+            fscanf(inp, "%lf", &a[i][j]);
             // храним по строкам
     double *dev_a;
     CSC(cudaMalloc( (void**) &dev_a, sizeof(double) * MAX_N * MAX_N));
@@ -54,30 +57,16 @@ int main() {
     std::iota(pos, pos+n, 0);
     int sign = 1;
     double det = 0;
+    int* it;
+    int major;
     for (int i = 0; i < n; ++i) {
         // выбираем максимальный
         cur_i = i;
-        int* it;
         it = std::max_element(pos+i, pos+n, cmp);
-        int major = it - pos;
-
-
-        // printf("%d %d %lf\n", i, major, a[major][i]);
-        if (i == n-1){
-            // если последняя строка
-            if (abs(a[i][i]) < eps ){
-                printf("det %.10e\n", 0.);
-                return 0;
-            }
-            else if (a[i][i] < 0){
-                det += log( -a[i][i] );
-                sign = - sign;
-            } else
-                det += log( a[i][i] );
-            break;
-        }
+        major = it - pos;
         CSC(cudaMemcpyToSymbol(cur_row, a[major], sizeof(double) * n, 0, cudaMemcpyHostToDevice));
 
+        // printf("%d %d %lf\n", i, major, a[major][i]);
         if (abs(a[major][i]) < eps ){
             printf("det %.10e\n", 0.);
             return 0;
@@ -86,21 +75,22 @@ int main() {
             sign = - sign;
         } else
             det += log( a[major][i] );
+        if (i == n-1)
+            break;
 
         if (major != i){
-            memcpy((double*)a + major * MAX_N,  (double*)a + i * MAX_N, MAX_N * sizeof(double));
+            memcpy((double*)a + major * MAX_N + i,  (double*)a + i * MAX_N + i, (n-i) * sizeof(double));
             sign = -sign;
         }
-        // CSC(cudaMemcpy(dev_a + i * MAX_N, a + i * MAX_N, sizeof(double) * MAX_N * (MAX_N - i), cudaMemcpyHostToDevice));
-        CSC(cudaMemcpy(dev_a, a, sizeof(double) * MAX_N * MAX_N, cudaMemcpyHostToDevice));
-        // printf("aaaaa\n");
+        CSC(cudaMemcpy((double*)dev_a + i * MAX_N, (double*)a + i * MAX_N, sizeof(double) * MAX_N * (n - i), cudaMemcpyHostToDevice));
+        // CSC(cudaMemcpy(dev_a, a, sizeof(double) * MAX_N * MAX_N, cudaMemcpyHostToDevice));
 
         //вычитаем из каждой строчки текущую
         kernel <<< dim3(32, 32), dim3(32, 32) >>> (dev_a, i, n);
-
         CSC(cudaGetLastError());
-        // CSC(cudaMemcpy((double*)a + i * MAX_N, (double*) dev_a + i * MAX_N, sizeof(double) * MAX_N * (MAX_N - i), cudaMemcpyDeviceToHost));
-        CSC(cudaMemcpy(a, dev_a, sizeof(double) * MAX_N * MAX_N, cudaMemcpyDeviceToHost));
+
+        CSC(cudaMemcpy((double*)a + i * MAX_N, (double*) dev_a + i * MAX_N, sizeof(double) * MAX_N * (n - i), cudaMemcpyDeviceToHost));
+        // CSC(cudaMemcpy(a, dev_a, sizeof(double) * MAX_N * MAX_N, cudaMemcpyDeviceToHost));
 
     }
     double ans = sign*exp(det);
