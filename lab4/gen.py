ef5853b54b95a123b9d18bdfcf8cb65f238983f5
Konstantin <russiankoteyka@gmail.com>
2019-12-16 04:45:24 +0300

    working with major and thrust / no bank conflicts resolve

--- lab4/gen.py
+++ lab4/gen.py
@@ -1,10 +1,11 @@
 import warnings
 import numpy as np
 
-n = 300
+n = 1000
+
 
 print(n)
-a = np.random.uniform(0, 1, (n, n))
+a = np.random.uniform(-0.2, 0.1, (n, n))
 for i in range(n):
     for j in range(n):
         print(a[i][j], end=' ')
--- lab4/memcpy.cpp
+++ lab4/memcpy.cpp
@@ -2,16 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int a[4][4];
+int a[2][6];
 // int b[10];
 int main(int argc, char const *argv[]) {
-    std::iota((int*)a, (int*)a+16, 0);
+    std::iota((int*)a, (int*)a+12, 0);
     // std::iota(b, b+10, 0);
 
-    memcpy((int*)a, (int*)a+5, 5*sizeof(int));
-    for (size_t i = 0; i < 16; i++) {
-        printf("%d ", ((int*)a)[i]);
+    // memcpy((int*)a, (int*)a+5, 5*sizeof(int));
+    for (size_t y = 0; y < 6; y++) {
+        for (size_t x = 0; x < 2; x++) {
+        printf("%d ", a[x][y]);
     }
     printf("\n");
+    }
+    // printf("\n");
+    printf("%d\n", ((int*)a)[3*2 + 1]);
     return 0;
 }
