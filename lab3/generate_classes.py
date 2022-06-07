b96042140bf5e6de693a285c57efd80b8b24749e
Sourcery AI <>
2022-06-07 21:19:46 +0000

    'Refactored by Sourcery'

--- lab3/generate_classes.py
+++ lab3/generate_classes.py
@@ -7,6 +7,6 @@ wh = 100
 if __name__ == '__main__':
     import numpy as np
     print(n_cls)
-    for i in range(n_cls):
+    for _ in range(n_cls):
         a = np.random.randint(0, wh-1, n_pxl);
         print(*a)
